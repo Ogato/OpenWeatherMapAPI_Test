6cee56aaa0d374e7edb61df0efaa3b848c91c337
Jason Ogato <jason.ogato@bison.howard.edu>
2017-07-23 15:01:07 -0700

    Changed layout for multiple forecasts

--- app/src/main/java/com/jogato/openweathermaptest/MainActivity.java
+++ app/src/main/java/com/jogato/openweathermaptest/MainActivity.java
@@ -1,136 +1,207 @@
 package com.jogato.openweathermaptest;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.location.Location;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import com.google.gson.stream.JsonReader;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mLocation;
-    private TextView mTemp;
-    private TextView mDescription;
-    private EditText mCity;
-    private String mSelectedCountry;
-    private String mSelectedCity;
-    private static final String API_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-    private static final String API_KEY = "2685bf8ee586a5edb31c0706655832bc";
+    private Button mOpenWeatherMap;
+    private Button mAPIXU;
+    private Button mSearch;
+    private TextView mWarn;
+    private EditText mGetCity;
+    private ProgressBar mProgress;
+    private String mCity;
+    private long mCityId;
+    private Map<String, CityInfo>cityInfoMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocation = (TextView) findViewById(R.id.location);
-        mTemp = (TextView) findViewById(R.id.temp);
-        mDescription = (TextView) findViewById(R.id.description);
-        mCity = (EditText) findViewById(R.id.city);
-        mSelectedCountry = "us";
-        mSelectedCity = "MountainView";
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.country_array, R.layout.array_layout);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner spinner = (Spinner) findViewById(R.id.countries);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        mCity = "";
+        cityInfoMap = new HashMap<String, CityInfo>();
+        mWarn = (TextView) findViewById(R.id.warning);
+
+        mProgress = (ProgressBar) findViewById(R.id.progress);
+        mOpenWeatherMap = (Button) findViewById(R.id.open_weather);
+        mAPIXU = (Button) findViewById(R.id.apixu);
+        mSearch = (Button) findViewById(R.id.search_city);
+
+        mGetCity = (EditText) findViewById(R.id.city);
+        mGetCity.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                mSelectedCountry = adapterView.getItemAtPosition(i).toString();
-                mSelectedCity = mCity.getText().toString().replace(" ", "");
-                new WeatherInfo().execute();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(mWarn.getVisibility() == View.VISIBLE) {
+                    mWarn.setVisibility(View.INVISIBLE);
+                }
+            }
 
+            @Override
+            public void afterTextChanged(Editable editable) {
+                mCity = editable.toString();
+                mGetCity.clearFocus();
             }
         });
+
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!mCity.isEmpty() && cityInfoMap.isEmpty()){
+                    mProgress.setVisibility(View.VISIBLE);
+                    mOpenWeatherMap.setEnabled(false);
+                    mAPIXU.setEnabled(false);
+                    mGetCity.setEnabled(false);
+                    new GetCityID().execute(mCity);
+                }
+                else if (mCity.isEmpty()){
+                    mWarn.setText("No city entered...");
+                    mWarn.setVisibility(View.VISIBLE);
+                }
+                else{
+                    mCity = mCity.toLowerCase().replace(" ", "");
+                    mCityId = cityInfoMap.get(mCity.toLowerCase().replace(" ", "")).getId();
+                    mProgress.clearFocus();
+                    mWarn.setText("City Found!");
+                    mWarn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        mOpenWeatherMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWarn.setVisibility(View.INVISIBLE);
+                Intent i = new Intent(MainActivity.this, OpenWeatherMap.class);
+                i.putExtra(OpenWeatherMap.CITY_KEY, mCityId);
+                startActivity(i);
+            }
+        });
+
+        mAPIXU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWarn.setVisibility(View.INVISIBLE);
+                Intent i = new Intent(MainActivity.this, APIXU.class);
+                i.putExtra(APIXU.CITY_KEY, mCity);
+                startActivity(i);
+            }
+        });
+
+
     }
 
+    class CityInfo{
+        long id;
+        String cityName;
+        String country;
 
-
-
-    private class WeatherInfo extends AsyncTask<Void, Void, JSONObject> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Log.i("JO_INFO", "JUST_EXECUTED");
+        public CityInfo(long _id, String _cityName, String _country){
+            this.id = _id;
+            this.cityName = _cityName.toLowerCase().replace(" ", "");
+            this.country = _country;
         }
 
+        public long getId() {
+            return id;
+        }
+
+        public String getCityName() {
+            return cityName;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+
+    }
+
+    private class GetCityID extends AsyncTask<String, Void, Map<String, CityInfo>> {
+
         @Override
-        protected JSONObject doInBackground(Void... voids) {
+        protected Map<String, CityInfo> doInBackground(String... strings) {
             try{
-                Log.i("LOG_URL", API_URL + mSelectedCity + "," + mSelectedCountry + "&" + "APPID=" + API_KEY);
-                URL url = new URL(API_URL + mSelectedCity + "," + mSelectedCountry + "&" + "APPID=" + API_KEY);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.connect();
-                try {
-                    InputStream inputStream = urlConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while((line = bufferedReader.readLine()) != null){
-                        stringBuilder.append(line);
+                InputStream is = MainActivity.this.getAssets().open("city.list.json");
+                JsonReader jsonReader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                jsonReader.beginArray();
+                while (jsonReader.hasNext()) {
+                    CityInfo c = readMessage(jsonReader);
+                    if(!cityInfoMap.containsKey(c.getCityName())) {
+                        cityInfoMap.put(c.getCityName(), c);
                     }
-                    bufferedReader.close();
-                    Log.i("JO_RESULTS", stringBuilder.toString());
-                    return new JSONObject(stringBuilder.toString());
-                }finally {
-                    urlConnection.disconnect();
-                    Log.i("JO_INFO", "DONE");
                 }
-
+                jsonReader.endArray();
+                jsonReader.close();
+            }catch (IOException ioe){
+                ioe.printStackTrace();
             }
-            catch (Exception e){
-                Log.i("JO_ERROR", e.toString());
-                return null;
-            }
-
+            return cityInfoMap;
         }
 
-        protected void onPostExecute(JSONObject response) {
-            if(response == null){
-                Log.i("JO_ERROR", "THERE WAS AN ERROR");
-            }
-            else{
-                try {
-                    Double tempInK = response.getJSONObject("main").getDouble("temp");
-                    Double localTempRep = (tempInK * 1.8) - 459.67;
-                    mTemp.setText("" +  localTempRep);
-                    mLocation.setText(response.getString("name"));
-                    mDescription.setText(response.getJSONArray("weather").getJSONObject(0).getString("description"));
-                }catch (JSONException e){
-                    Log.e("App", "Failure", e);
+        @Override
+        protected void onPostExecute(Map<String, CityInfo> cityInfo) {
+            super.onPostExecute(cityInfo);
+            mProgress.setVisibility(View.INVISIBLE);
+            mOpenWeatherMap.setEnabled(true);
+            mAPIXU.setEnabled(true);
+            mGetCity.setEnabled(true);
+            mCityId = cityInfo.get(mCity.toLowerCase().replace(" ", "")).getId();
+            mWarn.setText("City Found!");
+            mWarn.setVisibility(View.VISIBLE);
+        }
+
+        CityInfo readMessage(JsonReader reader) throws IOException {
+            long id = -1;
+            String cityName = null;
+            String country = null;
+            List<Double> geo = null;
+
+            reader.beginObject();
+            while (reader.hasNext()) {
+                String name = reader.nextName();
+                if (name.equals("id")) {
+                    id = reader.nextLong();
+                } else if (name.equals("name")) {
+                    cityName = reader.nextString();
+                } else if(name.equals("country")){
+                    country = reader.nextString();
+                } else {
+                    reader.skipValue();
                 }
             }
-
+            reader.endObject();
+            return new CityInfo(id, cityName, country);
         }
+
     }
 
 }
